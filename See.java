082b04eb73730482650b931b624e06b19dabe940
arunprakasam1994 <arunprakasam1994@gmail.com>
2022-02-16 17:09:47 +0530

    updated as the pgm in the updation

---
+++ See.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class See {
+	public static void main(String[] args) {
+	  Scanner scan=new Scanner(System.in);
+		
+		
+for(int i=0;i<6;i++) {
+	int x=5;
+	int y=10;
+	int z=x>y;
+	System.out.println("the result is"z=x>y);
+	
+	//System.out.println("enter your tamil mark");
+	//int tamil=Integer.parseInt(scan.nextLine());
+	//System.out.println("enter your english mark");
+	//int english=Integer.parseInt(scan.nextLine());
+	//System.out.println("enter your maths mark");
+	//int maths=Integer.parseInt(scan.nextLine());
+	//System.out.println("enter your science mark");
+	//int science=Integer.parseInt(scan.nextLine());
+	//System.out.println("enter your ss mark");
+	//int ss=Integer.parseInt(scan.nextLine());
+	
+	//System.out.println("The final output is"+(tamil+english+maths+science+ss));
+	
+	
+	
+}
+}
+}
