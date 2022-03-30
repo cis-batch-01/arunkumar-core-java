55f88d7c7a61248b4bfdec25757da91648416d42
arunprakasam1994 <arunprakasam1994@gmail.com>
2022-03-30 18:11:18 +0530

    sql basic operations

--- 29.03.2022.sql
+++ 29.03.2022.sql
@@ -19,11 +19,11 @@ insert into organization values('org03','2003-02-25','2004-03-20','hcl','www.hcl
 insert into organization values('org04','2004-04-20','2005-03-25','vitech','www.vitech.com','vitech.in','img5','INTERNAL') ;
 insert into organization values('org05','2005-02-20','2006-03-25','intel','www.intel.com','intel.in','img3','full') ;
 select * from organization;
-insert into instructor (id,created_at,updated_at,bio,tagline,user_id)values('ins1','2019-09-06','2019-06-09','User','user_01','user1');
-insert into instructor (id,created_at,updated_at,bio,tagline,user_id)values('ins2','2019-09-06','2019-06-09','User','user_02','user2');
-insert into instructor (id,created_at,updated_at,bio,tagline,user_id)values('ins3','2019-09-06','2019-06-09','User','user_01','user3');
-insert into instructor (id,created_at,updated_at,bio,tagline,user_id)values('ins4','2019-09-06','2019-06-09','User','user_01','user4');
-insert into instructor (id,created_at,updated_at,bio,tagline,user_id)values('ins5','2019-09-06','2019-06-09','User','user_01','user5');
+insert into instructor (id,created_at,updated_at,bio,tagline,user_id)values('ins1','2019-09-06','2019-06-09','User','user_01','user01');
+insert into instructor (id,created_at,updated_at,bio,tagline,user_id)values('ins2','2019-09-06','2019-06-09','User','user_02','user02');
+insert into instructor (id,created_at,updated_at,bio,tagline,user_id)values('ins3','2019-09-06','2019-06-09','User','user_01','user03');
+insert into instructor (id,created_at,updated_at,bio,tagline,user_id)values('ins4','2019-09-06','2019-06-09','User','user_01','user04');
+insert into instructor (id,created_at,updated_at,bio,tagline,user_id)values('ins5','2019-09-06','2019-06-09','User','user_01','user05');
 select * from instructor;
 insert into user(id,created_at,updated_at,first_name,last_name,email,mobile,password,date_of_birth,address,display_picture_id,organization_id)
 values('user01','2020-04-09','2020-04-10','Arunkumar','G','arun@gmail.com',9845318551,'arun@123','1997-04-09','{ "door_no":63,"street":"Rajajistreet","city":"arakkonam","State":"TN"}','img1','org01');
@@ -36,18 +36,74 @@ values('user04','2020-04-09','2020-04-10','Arunkumar','l','larun@gmail.com',9845
 insert into user(id,created_at,updated_at,first_name,last_name,email,mobile,password,date_of_birth,address,display_picture_id,organization_id)
 values('user05','2020-04-09','2020-04-10','Arun','G','arun@gmail.com',9845318551,'arun@123','1997-04-09','{ "door_no":69,"street":"Rajajistreet","city":"arakkonam","State":"TN"}','img5','org05');
 select * from user;
-insert into role values('r001','2000-02-20','2005-03-25','student',' recent learning ','1','1','org01','k001','accept') ;
+insert into role values('r001','2000-02-20','2005-03-25','instructor',' recent learning ','1','1','org01','k001','accept') ;
 insert into role values('r002','2000-02-23','2005-03-20','alumni',' already learned student ','0','0','org02','k002','accept') ;
 insert into role values('r003','2000-02-24','2005-03-25','admin',' office work ','1','1','org03','k003','accept') ;
 insert into role values('r004','2000-02-21','2005-03-24','student',' recent learning ','1','1','org04','k004','accept') ;
-insert into role values('r005','2000-02-23','2005-03-27','alumni',' office work ','1','1','org05','k005','accept') ;
+insert into role values('r005','2000-02-23','2005-03-27','staff',' office work ','1','1','org05','k005','accept') ;
 select * from role;
+delete from  role;
+set sql_safe_updates=0;
 insert into user_roles(`user_id`,`role_id`)values('user01','r001');
 insert into user_roles(`user_id`,`role_id`)values('user02','r002');
 insert into user_roles(`user_id`,`role_id`)values('user03','r003');
 insert into user_roles(`user_id`,`role_id`)values('user04','r004');
 insert into user_roles(`user_id`,`role_id`)values('user05','r005');
 select * from user_roles;
+
+
+select * from course;
+desc course;
+select * from course;
+insert into course (id,created_at,updated_at,title,description,status,duration,
+level,price,rating,rating_count,instructor_id,code,banner_id,cover_id,curriculum,visibility) 
+values ('c001','2022-02-20','2022-02-22','HTML','hypertext markup language','DRAFT','3h','BEGINNER',5000,4,0,'ins1','html01','img1','img6','{"Topics":"HTML tags",
+"Topics":"HTML links","Topics":"HTML forms","Topics":"HTML comments","Topics":"HTML tables","Topics":"HTML list"}','PUBLIC');
+insert into course (id,created_at,updated_at,title,description,status,duration,
+level,price,rating,rating_count,instructor_id,code,banner_id,cover_id,curriculum,visibility) 
+values ('c003','2022-02-20','2022-02-22','HTML','hypertext markup language','DRAFT','3h','intermediate',5000,4,0,'ins3','java','img3','img5','{"Topics":"HTML tags",
+"Topics":"HTML links","Topics":"HTML forms","Topics":"HTML comments","Topics":"HTML tables","Topics":"HTML list"}','PUBLIC');
+insert into course (id,created_at,updated_at,title,description,status,duration,
+level,price,rating,rating_count,instructor_id,code,banner_id,cover_id,curriculum,visibility) 
+values ('c005','2022-02-20','2022-02-22','HTML','hypertext markup language','DRAFT','3h','intermediate',5000,4,0,'ins5','css','img4','img2','{"Topics":"HTML tags",
+"Topics":"HTML links","Topics":"HTML forms","Topics":"HTML comments","Topics":"HTML tables","Topics":"HTML list"}','PUBLIC');
+insert into course (id,created_at,updated_at,title,description,status,duration,
+level,price,rating,rating_count,instructor_id,code,banner_id,cover_id,curriculum,visibility) 
+values ('c002','2022-02-20','2022-02-22','HTML','hypertext markup language','DRAFT','3h','Beginner',5000,4,0,'ins3','css','img2','img3','{"Topics":"HTML tags",
+"Topics":"HTML links","Topics":"HTML forms","Topics":"HTML comments","Topics":"HTML tables","Topics":"HTML list"}','PUBLIC');
+insert into course (id,created_at,updated_at,title,description,status,duration,
+level,price,rating,rating_count,instructor_id,code,banner_id,cover_id,curriculum,visibility) 
+values ('c004','2022-02-20','2022-02-22','HTML','hypertext markup language','DRAFT','3h','advanced',5000,4,0,'ins4','java','img6','img4','{"Topics":"HTML tags",
+"Topics":"HTML links","Topics":"HTML forms","Topics":"HTML comments","Topics":"HTML tables","Topics":"HTML list"}','PUBLIC');
+insert into course (id,created_at,updated_at,title,description,status,duration,
+level,price,rating,rating_count,instructor_id,code,banner_id,cover_id,curriculum,visibility) 
+values ('c006','2022-02-20','2022-02-22','HTML','hypertext markup language','published','3h','Beginner',5000,4,0,'ins3','css','img3','img3','{"Topics":"HTML tags",
+"Topics":"HTML links","Topics":"HTML forms","Topics":"HTML comments","Topics":"HTML tables","Topics":"HTML list"}','PUBLIC');
+select * from course;
+delete from course;
+ insert into category_courses( `category_id`,`course_id` )values('cat1','c001');
+insert into category_courses( `category_id`,`course_id` )values('cat2','c002');
+insert into category_courses( `category_id`,`course_id` )values('cat3','c001');
+ insert into category_courses( `category_id`,`course_id` )values('cat4','c005');
+insert into category_courses( `category_id`,`course_id` )values('cat5','c005');
+select * from category_courses;
+insert into course_students (course_id,user_id) values ('c001','user01');
+insert into course_students (course_id,user_id) values ('c002','user02');
+insert into course_students (course_id,user_id) values ('c003','user03');
+insert into course_students (course_id,user_id) values ('c004','user04');
+insert into course_students (course_id,user_id) values ('c005','user05');
+insert into course_students (course_id,user_id) values ('c005','user06');
+insert into course_students (course_id,user_id) values ('c004','user01');
+select * from course_students;
+update course_students set course_id='c001' where user_id='user02';
+delete from  course_students;
+ insert into course_pre_requisites(`course_1_id`,`course_2_id`)values('c001','c002');
+insert into course_pre_requisites(`course_1_id`,`course_2_id`)values('c002','c003');
+insert into course_pre_requisites(`course_1_id`,`course_2_id`)values('c001','c003');
+insert into course_pre_requisites(`course_1_id`,`course_2_id`)values('c004','c004');
+insert into course_pre_requisites(`course_1_id`,`course_2_id`)values('c005','c005');
+select * from course_pre_requisites;
+
 
 
 
---
+++ question.sql
@@ -0,0 +1,78 @@
+/*1. Write a query to display all the available course categories?*/
+
+select title,description,created_at from category;
+
+/*2.write a query to get the Organization title and website Address.*/
+
+select name,website from Organization;
+select * from role;
+
+/*3.write a query to get coursename, count of students in each course*/
+
+select * from course;
+select * from course_students;
+select course.title,count(user_id) as Count from course
+join course_students as cs on course.id=cs.course_id
+group by course.title;
+
+/*4. write a query to get username ,email, role of all the users*/
+select * from user;
+select * from user_roles;
+select * from role;
+select first_name,last_name,email,role.title from user
+join user_roles on user_roles.user_id=user.id
+join role on role.id=user_roles.role_id;
+
+/*5. write a query to show the all the sub categiries of category 'frontEnd';*/
+
+select * from category where parent_id in (select id from category where title="front-end");
+
+/*6.write a query to show the name of the course,no of Students in course java*/
+
+select * from course;         
+select * from course_students;
+select course.title,count(user_id)Student_List from course
+join course_students as cs on course.id=cs.course_id where title="java";
+
+/*7.write a query to show all the course details of Back End*/
+
+select * from category where  title="back-end";
+
+/*8.Write a query to show the payment details*/
+
+
+
+/*9.Write a query to course name and instructor name*/
+
+select * from instructor;
+select * from role;
+select * from course;
+desc course;
+select * from user;
+select * from organization;
+
+select title,user.first_name from course
+join instructor on  course.instructor_id=instructor.id
+join user on instructor.user_id=user.id;
+
+/*10.Write a query to get all the course under 20,000.*/
+
+select course.price from course where price<20000;
+
+/*11.write a query to fetch all the corses for the user from organization org05*/
+
+select * from course_students 
+join course_students on course_students.course_id=course.id
+join user on user.id=course_students.user_id 
+join organization on user.organization_id=organization.id
+where organization.id="org05";/*license*/
+/*12.write a query to display only the admins name and email ,permissions*/
+select user.first_name,user.email,role.permissions from role join organization on role.organization_id=organization.id
+join user on organization.id=user.organization_id where role.title="student";
+/*13.write a query to display all  courses published and its category name*/
+select course.status,category.title from course
+join category_courses as cc on course.id=cc.course_id
+join category on cc.category_id=category.id
+where course.status="draft";
+/*14.write a query to display all advanced courese*/
+select course.title from course where level="advanced";
